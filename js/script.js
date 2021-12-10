82938c3e29fc6d77125e697d88fc94fdb87b45f4
KCorigliano <corigliano98@hotmail.com>
2021-12-10 13:53:45 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const rowContainer = document.querySelector('.row');
 
 for (i=1; i<=100; i++){
-    rowContainer.innerHTML += `<span>${i}</span>`
+
+    if (i % 5 === 0 && i % 3 === 0){
+        rowContainer.innerHTML += '<span>FizzBuzz</span>';
+    }
+    else if (i % 5 === 0){
+        rowContainer.innerHTML += '<span>Fizz</span>';
+    } else if (i % 3 === 0){
+        rowContainer.innerHTML += '<span>Buzz</span>';
+    } else{
+        rowContainer.innerHTML += `<span>${i}</span>`;
+    }
+    
 }
